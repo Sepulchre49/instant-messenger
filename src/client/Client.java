5f805134e4bdd0826161cadd92e20e001ff1c78f
CalebF98 <cfringer98@gmail.com>
2024-04-15 22:05:40 -0700

    Created Client driver to test login functionality.

--- src/client/Client.java
+++ src/client/Client.java
@@ -1,7 +1,32 @@
 package client;
 
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+
+import shared.Message;
+
 public class Client {
-	public static void main(String[] args) {
-		System.out.println("Hello from the client!");
+    public static void main(String[] args) {
+	System.out.println("Hello from the client!");
+	
+	Message loginRequest = new Message(Message.Type.LOGIN, Message.Status.REQUEST, "username: caleb password: letmein");
+
+	try {
+	    Socket s = new Socket("localhost", 3000);
+	    ObjectOutputStream out = new ObjectOutputStream(s.getOutputStream());
+	    ObjectInputStream in = new ObjectInputStream(s.getInputStream());
+
+	    out.writeObject(loginRequest);
+	    Message res = (Message) in.readObject();
+	    System.out.println(res.getContent());
+
+	    in.close();
+	    out.close();
+	    s.close();
+	} catch (IOException | ClassNotFoundException e) {
+	    e.printStackTrace();
 	}
+    }
 }
