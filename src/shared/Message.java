03e3d1b76e84d265f513b369e315942e2db75bbf
Caleb Fringer <cfringer98@gmail.com>
2024-04-15 19:06:22 -0700

    Added enough of the Message skeleton to make the server compile.

--- src/shared/Message.java
+++ src/shared/Message.java
@@ -1,5 +1,38 @@
 package shared;
 
 public class Message {
-	
+    public enum Type {
+	LOGIN,
+	LOGOUT,
+	TEXT
+    }
+
+    public enum Status {
+	SUCCESS,
+	FAILURE,
+	ERROR,
+	INVALID_ARGUMENT
+    }
+
+    private Type type;
+    private Status status;
+    private String content;
+
+    public Message(Type t, Status s, String msg) {
+	this.type = t;
+	this.status = s;
+	this.content = msg;
+    }
+
+    public Type getType() {
+	return type;
+    }
+
+    public Status getStatus() {
+	return status;
+    }
+
+    public String getContent() {
+	return content;
+    }
 }
