bff35bda423663a2ca4193316020712ca429d177
miaaax <107900772+miaaax@users.noreply.github.com>
2024-04-16 18:07:48 -0700

    Updated ClientUser

--- src/client/ClientUser.java
+++ src/client/ClientUser.java
@@ -1,5 +1,39 @@
 package client;
+import java.util.Set;
 
 public class ClientUser {
+	private int userId;
+	private String username;
+	private Set<Conversation> convrsations;
+	
+	public ClientUser(int userId, String username){ 
+		this.userId = userId;
+		this.username = username;
+	}
+	
+	public int getUserId() {
+		return userId;
+	}
+	
+	public String getUsername() {
+		return username;
+	}
+	
+	public boolean login() {
+		return true;
+	}
+	
+	public void receive(Message message) {
+		
+	}
+	
+	public void logout() {
+		
+	}
+	
+	public void loadConversation() {
+		
+	}
 
 }
+
