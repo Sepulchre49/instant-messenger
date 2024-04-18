e55093cb71822d7d6d13018200f0fffcbd9e9069
CalebF98 <cfringer98@gmail.com>
2024-04-17 19:17:00 -0700

    Fixed indentation

--- src/client/ClientUser.java
+++ src/client/ClientUser.java
@@ -2,38 +2,38 @@ package client;
 import java.util.Set;
 
 public class ClientUser {
-	private int userId;
-	private String username;
-	private Set<Conversation> convrsations;
-	
-	public ClientUser(int userId, String username){ 
-		this.userId = userId;
-		this.username = username;
-	}
-	
-	public int getUserId() {
-		return userId;
-	}
-	
-	public String getUsername() {
-		return username;
-	}
-	
-	public boolean login() {
-		return true;
-	}
-	
-	public void receive(Message message) {
-		
-	}
-	
-	public void logout() {
-		
-	}
-	
-	public void loadConversation() {
-		
-	}
+    private int userId;
+    private String username;
+    private Set<Conversation> convrsations;
+    
+    public ClientUser(int userId, String username){ 
+        this.userId = userId;
+        this.username = username;
+    }
+    
+    public int getUserId() {
+        return userId;
+    }
+    
+    public String getUsername() {
+        return username;
+    }
+    
+    public boolean login() {
+        return true;
+    }
+    
+    public void receive(Message message) {
+            
+    }
+    
+    public void logout() {
+            
+    }
+    
+    public void loadConversation() {
+            
+    }
 
 }
 
