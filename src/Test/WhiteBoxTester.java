9f872d4eac88d73a16a06147c8824e31e94df09d
Quang Nguyen <nguyennhatq22@gmail.com>
2024-04-25 13:59:48 -0700

    WhiteBoxTest created and still working..

---
+++ src/Test/WhiteBoxTester.java
@@ -0,0 +1,15 @@
+package Test;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+//import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+
+@Suite.SuiteClasses({
+	ClientTester.class, 
+	ServerUserTester.class,
+	MessageTester.class
+})
+public class WhiteBoxTester {
+}
