99e48b5fe277c6b6557ce1855d5c7f7b84c7326e
Caleb <cfringer98@gmail.com>
2024-04-27 14:21:22 -0700

    Converted connection test to constructor test since doMessageLoop is gone.

--- src/Test/ClientTester.java
+++ src/Test/ClientTester.java
@@ -8,24 +8,8 @@ import java.io.IOException;
 
 
 public class ClientTester {
-	
 	@Test
-	public void testConnectionandMessageLoop() {
-		Client client = new Client();
-		
-		try {
-			client.connectToServer();
-		} catch(IOException e) {
-			fail("Error: Not Able To Connect To Server");
-		}
-		
-		try {
-			client.doMessageReadLoop();
-		}catch(IOException | ClassNotFoundException e) {
-			fail("Error: Error In Message Loop");
-		}catch(Exception e) {
-			fail("Error: Unexpected Error: " + e.getMessage());
-		}
+	public void constructor() {
+        assertNotNull(new Client());
 	}
-
 }
