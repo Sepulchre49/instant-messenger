b22832f8c8cfe29bbfe9980f3409454d6b33aa82
Quang Nguyen <nguyennhatq22@gmail.com>
2024-04-28 10:25:17 -0700

    Notes for some test case for BlackBoxTester

---
+++ src/Test/BlackBoxTester.java
@@ -0,0 +1,34 @@
+package Test;
+
+public class BlackBoxTester {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//Case 1: Check Server connectivity
+		
+		
+		
+		//Case 2: Client user enter username and password
+		
+		
+		//Case 3: Create new Conversation 
+		
+		
+		//Case 4: Texting message
+		
+		
+		//Case 5: Sending message
+		
+		
+		//Case 6: Receive Message 
+		
+		
+		//Case 7 : Create a Group Message 
+		
+		
+		//Case 8 : 
+
+	}
+
+}
