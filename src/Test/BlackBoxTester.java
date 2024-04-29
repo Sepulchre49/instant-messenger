1af4f3e5b39bb9d7e38eabf77f2ce15e1f4e9dfe
Quang Nguyen <nguyennhatq22@gmail.com>
2024-04-28 18:13:14 -0700

    Black Box Test: ServerUser Black Box Test

--- src/Test/BlackBoxTester.java
+++ src/Test/BlackBoxTester.java
@@ -1,33 +1,47 @@
 package Test;
 
+import java.io.File;
+import java.util.HashSet;
+import java.util.Set;
+
+import server.Conversation;
+import server.ServerUser;
+import shared.Message;
+
 public class BlackBoxTester {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("ServerUser Black Box Tester: ");
+		Set<ServerUser> participants = new HashSet<>();
 		
-		//Case 1: Check Server connectivity
+		participants.add(new ServerUser("user1", "12345"));
+		Conversation conversation = new Conversation(participants);
+		System.out.println("Create a new user: ");
+		//Create a new user
+		ServerUser user1= new ServerUser("quang","12345");
 		
+		//add conversation 
+		System.out.println("Add conversation: ");
+		user1.addConversation(conversation);
 		
+		//Receiving a Message 
+		System.out.println("Receiving a message:");
+		Message message = new Message(1,null,Message.Type.TEXT,Message.Status.REQUEST,"Test Receive Message", 1);
+		user1.receive(message);
 		
-		//Case 2: Client user enter username and password
+		//logging in 
+		user1.login();
 		
+		//logging out
+		user1.logout();
 		
-		//Case 3: Create new Conversation 
+		//print user information 
+		System.out.println("Show the user information:");
+		System.out.println("User ID: "+ user1.getUserId());
+		System.out.println( "Username: "+ user1.getUsername());
+		System.out.println("Is logged in: "+ user1.isLoggedIn());
+		System.out.println( "Inbox count: "+ user1.getInboxCount());
 		
-		
-		//Case 4: Texting message
-		
-		
-		//Case 5: Sending message
-		
-		
-		//Case 6: Receive Message 
-		
-		
-		//Case 7 : Create a Group Message 
-		
-		
-		//Case 8 : 
 
 	}
 
